ffb838a19b5c92e67363522aac9553d76545940a
Chamod Rashmika <cnilwakka@gmail.com>
2025-07-02 18:14:56 +0530

    Fix import case for Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import NavBar from "./components/NavBar";
-import Hero from "./components/hero";
+import Hero from "./components/Hero";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
